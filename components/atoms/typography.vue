3065f72358172bb66cb148071d7fe6f956de2c8c
Yelm PC <dj.marvels@gmail.com>
2021-02-22 21:38:28 +0300

    typography component

---
+++ components/atoms/typography.vue
@@ -0,0 +1,126 @@
+<template>
+  <span :class="textClass">
+    <slot name="default" />
+  </span>
+</template>
+
+<script>
+export default {
+  name: "typography",
+  computed: {
+    textClass () {
+      let textClass = (this.heading !== null) ? 'text heading-'+this.heading : (this.body !== null) ? 'text body-'+this.body : (this.lead !== null) ? 'text lead-'+this.lead : 'text'
+      if (this.color !== null) {
+        textClass += ' text-'+this.color
+      }
+      return textClass
+    }
+  },
+  props: {
+    heading: {
+      required: false,
+      default: null
+    },
+    body: {
+      required: false,
+      default: null
+    },
+    lead: {
+      required: false,
+      default: null
+    },
+    color: {
+      required: false,
+      default: null
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/colors.scss";
+.text {
+  font-style: normal;
+  font-display: swap;
+  font-feature-settings: 'pnum' on, 'lnum' on;
+  // Sizes
+  &.lead-14 {
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 24px;
+  }
+  &.heading-1 {
+    font-weight: 600;
+    font-size: 26px;
+    line-height: 40px;
+  }
+  &.heading-2 {
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 32px;
+  }
+  &.body-16 {
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 20px;
+  }
+  &.body-14 {
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 20px;
+  }
+  &.body-12 {
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 16px;
+  }
+  &.lead-14 {
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 24px;
+  }
+  // Colors
+  &-primary {
+    color: $primary;
+    &-100 {
+      color: $primary-200;
+    }
+    &-200 {
+      color: $primary-300;
+    }
+    &-300 {
+      color: $primary-400;
+    }
+    &-600 {
+      color: $primary-600;
+    }
+  }
+  &-grey {
+    color: $grey;
+    &-200 {
+      color: $grey-200;
+    }
+    &-300 {
+      color: $grey-300;
+    }
+    &-500 {
+      color: $grey-500;
+    }
+    &-600 {
+      color: $grey-600;
+    }
+  }
+  &-white {
+    color: $white;
+  }
+  &-success {
+    color: $success;
+  }
+  &-warning {
+    color: $warning;
+  }
+  &-error {
+    color: $error;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>
-    index play sell
+  <div class="ui">
+    <div class="ui-block">
+      <div class="ui-block-header"><typo heading="1">Typography</typo></div>
+      <div class="ui-block-body">
+        <div><typo heading="1" color="primary">Heading 1</typo></div>
+        <div><typo heading="2" color="grey">Heading 2</typo></div>
+        <div><typo body="16" color="success">Body 16</typo></div>
+        <div><typo body="14" color="error">Body 14</typo></div>
+        <div><typo body="12" color="warning">Body 12</typo></div>
+        <div><typo lead="14" color="primary-600">Lead 14</typo></div>
+      </div>
+    </div>
+    <div class="ui-block">
+      <div class="ui-block-header"><typo heading="1">Icons</typo></div>
+    </div>
   </div>
 </template>
 
 <script>
+import typo from '@/components/atoms/typography'
 export default {
-  name: "index"
+  name: "index",
+  components: {
+    typo,
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.ui {
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  &-block {
+    flex-basis: calc(100% / 3);
+    &-header {
+      margin-bottom: 24px;
+    }
+  }
+}
 </style>
