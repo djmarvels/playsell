56ceab6fcba45b2cbdef80c4d9c3830f15b7ea13
Yelm PC <dj.marvels@gmail.com>
2021-02-26 09:48:18 +0300

    update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 import webpack from 'webpack'
+const TerserPlugin = require('terser-webpack-plugin');
 export default {
   target: 'static',
   head: {
@@ -48,5 +49,72 @@ export default {
       file: { esModule: false },
       url: { esModule: false }
     },
+    postcss: {
+      plugins: {
+        cssnano: {
+          preset: [
+            'default',
+            {
+              discardComments: {
+                removeAll: true
+              }
+            }
+          ]
+        }
+      }
+    },
+    html: {
+      minify: {
+        collapseBooleanAttributes: true,
+        decodeEntities: true,
+        minifyCSS: true,
+        minifyJS: true,
+        processConditionalComments: true,
+        removeEmptyAttributes: true,
+        removeRedundantAttributes: true,
+        trimCustomFragments: true,
+        useShortDoctype: true
+      }
+    },
+    filenames: {
+      app: ({ isDev, isModern }) => isDev ? `[name]${isModern ? '.modern' : ''}.js` : `[contenthash:7]${isModern ? '.modern' : ''}.js`,
+      chunk: ({ isDev, isModern }) => isDev ? `[name]${isModern ? '.modern' : ''}.js` : `[contenthash:7]${isModern ? '.modern' : ''}.js`,
+      css: ({ isDev }) => isDev ? '[name].css' : 'css/[contenthash:7].css',
+      img: ({ isDev }) => isDev ? '[path][name].[ext]' : 'img/[name].[contenthash:7].[ext]',
+      font: ({ isDev }) => isDev ? '[path][name].[ext]' : 'fonts/[name].[contenthash:7].[ext]',
+      video: ({ isDev }) => isDev ? '[path][name].[ext]' : 'videos/[name].[contenthash:7].[ext]'
+    },
+    splitChunks: {
+      layouts: true
+    },
+    extractCSS: true,
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true, // Must be set to true if using source-maps in production
+          terserOptions: {
+            // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+          },
+        }),
+        // optimize-css-assets-webpack-plugin
+      ],
+      splitChunks: {
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            test: /\.(css|vue)$/,
+            chunks: 'all',
+            enforce: true
+          }
+        }
+      }
+    },
+    optimizeCSS: {
+      cssProcessor: require('css-mqpacker'),
+      cssProcessorPluginOptions: { sort: true }
+    }
   }
 }
